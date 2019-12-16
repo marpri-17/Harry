11a9b021f327fe463be213c554c020fd89053845
marpri-17 <mar.pnieto@gmail.com>
2019-12-16 12:45:57 +0100

    add style to card component

--- src/components/ResultCard.js
+++ src/components/ResultCard.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import { Card, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
 
 const ResultCard = ({ spell }) => {
+    const useStyles = makeStyles({
+        card: {
+            height: "140px"
+        }
+    })
+
+    const classes = useStyles();
+
     return (
-        <Card>
+        <Card className={classes.card}>
             <Typography>{spell.name}</Typography>
         </Card>)
 
---
+++ src/styles/styledCard.js
@@ -0,0 +1,11 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+    card: {
+        height: "140px"
+    }
+})
+
+export default function styledCard() {
+    const classes = useStyles();
+}
