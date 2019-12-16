dc71ca4cbe612d8ab6db0df24e5fcb94cdcea9ce
marpri-17 <mar.pnieto@gmail.com>
2019-12-16 19:43:52 +0100

    Add image and start logic to autoassign it

--- src/components/ResultCard.js
+++ src/components/ResultCard.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import charmPic from '../images/spellPic.jpg';
+import enchancementPic from '../images/enchancement.jpg';
 import { Card, CardMedia, Typography, CardActionArea } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
@@ -6,20 +8,31 @@ import { makeStyles } from '@material-ui/core/styles';
 const ResultCard = ({ spell }) => {
     const useStyles = makeStyles({
         card: {
-            height: "140px",
+
             maxWidth: "25%",
+        },
+        pic: {
+            height: 60
         }
     })
 
     const classes = useStyles();
 
+    const SelectPic = (spellType) =>
+        (spellType.toLowerCase() === "charm") ? true : false;
+
     return (
         <Card className={classes.card}>
             <CardActionArea>
+                <CardMedia
+                    className={classes.pic}
+                    image={SelectPic(spell.type) ? charmPic : enchancementPic}
+                    title="Encantamiento o hechizo"
+                />
                 <Typography component="h4" variant="h5">{spell.name}</Typography>
                 <Typography variant="subtitle1" paragraph={true}>{spell.type}</Typography>
             </CardActionArea>
-            <Typography variant="body2">{spell.description}</Typography>
+            <Typography variant="body2" component="p">{spell.description}</Typography>
         </Card>)
 }
 
--- src/components/Spells.js
+++ src/components/Spells.js
@@ -11,6 +11,7 @@ class Spells extends React.Component {
             spells: [],
             isLoading: true,
         };
+        this.renderSpellTypeImage = this.renderSpellTypeImage.bind(this);
 
     }
 
@@ -19,11 +20,31 @@ class Spells extends React.Component {
             .then(fetchedSpells => this.setState({
                 spells: fetchedSpells,
                 isLoading: false,
-            }));
+            }
+            ));
+
     }
+
+    renderSpellTypeImage() {
+        const { spells } = this.state;
+        let types = [];
+        for (let spell of spells) {
+            if (types.includes(spell.type.toLowerCase())) {
+            } else {
+                types.push(spell.type.toLowerCase())
+            }
+        }
+        console.log(types)
+    }
+
 
     render() {
         const { spells, isLoading } = this.state
+
+        if (spells.length !== 0) {
+            this.renderSpellTypeImage();
+        }
+
         return (
             <Paper component="section">
                 <Typography variant="h3">Hechizos</Typography>
@@ -32,6 +53,7 @@ class Spells extends React.Component {
                     {(isLoading) ?
                         <Typography variant="caption">"Cargando"</Typography> :
                         spells.map(spell => {
+                            debugger;
                             return (
                                 <ResultCard spell={spell} key={spell.id} />
                             )
